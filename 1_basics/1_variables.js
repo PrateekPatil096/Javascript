df3cf4603a361d8a066fe1e9975f59337b306400
Prateek Patil <prateekpatil096@gmail.com>
2026-03-08 22:56:50 +0530

    var,const,let

---
+++ 1_basics/1_variables.js
@@ -0,0 +1,11 @@
+const accountId=1445
+let accountEmail="pp@gmail.com"
+var accountPassword="4321"
+accountCity="jamakhandi"
+let accountState;
+
+accountEmail="p2p2p@gmail.com"
+accountPassword="1234"
+accountCity="jkd"
+
+console.table([accountId,accountPassword,accountEmail,accountCity,accountState])
