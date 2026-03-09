e0c676067d8563d5ab89d644f1969ede13cb5404
Prateek Patil <prateekpatil096@gmail.com>
2026-03-09 14:32:55 +0530

    operations added

--- 1_basics/3_conversionOperations.js
+++ 1_basics/3_conversionOperations.js
@@ -20,3 +20,34 @@ let someNumber=33
 let stringNumber= String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+// *********operations*******
+
+let value=3
+let negValue=-value
+console.log(negValue);
+
+let str1="hello"
+let str2="kakashi"
+let str3=str1 + str2
+console.log(str3);
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
+
+console.log(+true);
+console.log(+"");
+
+let gameCounter=100
+gameCounter++;
+console.log(gameCounter);
+
+let a=70;
+console.log(a++);
+console.log(a)
+
+let b=90;
+console.log(++b);
+console.log(b)
