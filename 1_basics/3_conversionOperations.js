c4ab7eb0115d2f51511ce22df982f9f97c357eae
Prateek Patil <prateekpatil096@gmail.com>
2026-03-09 12:17:02 +0530

    conversion and operations

---
+++ 1_basics/3_conversionOperations.js
@@ -0,0 +1,22 @@
+let score="33abc"
+console.log(typeof score);
+console.log(typeof (score));
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+let ff=null
+console.log(typeof ff);
+let valueInNum = Number(ff)
+console.log(typeof valueInNum);
+console.log(valueInNum);
+
+let isLoggedIn="naruto"
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+console.log(typeof booleanIsLoggedIn)
+
+let someNumber=33
+let stringNumber= String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
