f208e3adc9082dc4edddd906ff3ed0d2ed5a5085
Prateek Patil <prateekpatil096@gmail.com>
2026-03-09 22:10:34 +0530

    summary of datatype

---
+++ 1_basics/datatypes_summary.js
@@ -0,0 +1,22 @@
+const score=100
+const scoreValue=100.3
+const isLoggedIn=false
+const outsideTemp=null
+let userEmail;
+const id=Symbol('123')
+const anotherId=Symbol('123')
+console.log(id===anotherId);
+const bigNumber=34564567856789n
+
+const heros=["toji" , "gojo" ,"yuta"];
+
+let myObj={
+    name:"ppp",
+    age:19,
+}
+
+const myFunction=function(){
+    console.log("hello world");
+
+}
+console.log(typeof id)
