21a7c3d54c205f0c3ce5ac5ad12a19b82ec77bf9
Prateek Patil <prateekpatil096@gmail.com>
2026-03-09 11:05:15 +0530

    datatypes

---
+++ 1_basics/2_datatypes.js
@@ -0,0 +1,21 @@
+console.log(3+3)
+console.log("toji")
+
+let name="gojo"
+let age=19
+let isLoggedIn=false
+let state=null
+
+//number=> 2to power 53
+//bigint
+//string=>""
+//boolean=>true/flase
+//null=> standardalone value
+//undefined=>
+
+//symbol=>unique
+//object
+
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined);
