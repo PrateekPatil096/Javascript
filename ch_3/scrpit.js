b75bcd7453081aec18282f54c62c81ae27213d0d
Prateek Patil <prateekpatil096@gmail.com>
2026-07-22 15:26:40 +0530

    ch_3

---
+++ ch_3/scrpit.js
@@ -0,0 +1,31 @@
+// var x=7;
+// function getname(){
+//     console.log("namaste js");
+// }
+// getname();
+// console.log(x);
+
+// getname();
+// console.log(x);
+// var x=7;
+
+// function getname(){
+//     console.log("namaste js");
+// }
+
+// var x=7;
+// function getname(){
+//     console.log("namaste js");
+// }
+// console.log(getname);
+
+getname();
+console.log(x);
+console.log(getname);
+
+var x=7;
+function getname(){
+    console.log("namaste js");
+
+    
+}
