adcc9be1a60c413815ddc63b482d44d166b0210a
Prateek Patil <prateekpatil096@gmail.com>
2026-07-24 10:26:58 +0530

    undefined vs not defined in js

---
+++ scrpit.js
@@ -0,0 +1,20 @@
+console.log(a);
+var a=7;
+console.log(a);
+
+var b;
+console.log(b);
+b=10;
+if(b===undefined){
+    console.log("b is undefined")
+}
+else{
+    console.log("b is not undefined")
+}
+
+var x;
+console.log(x);
+x=10;
+console.log(x);
+x="hello world";
+console.log(x);
