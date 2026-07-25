5ddd0ef69f22d16ceff51c058064e5decbf3b010
Prateek Patil <prateekpatil096@gmail.com>
2026-07-25 13:15:00 +0530

    lexical

--- ch_6/scrpit.js
+++ ch_6/scrpit.js
@@ -7,9 +7,23 @@ a();
 function x(){
     c();
     function c(){
-        console.log(b);
+        console.log(z);
 
     }
 }
-var b=10;
-a();
+var z=10;
+x();
+var h =0
+ function u(){
+    h =2
+    v()
+    function v(){
+        p()
+        h=3
+        
+        function p(){
+            console.log(h)
+        }
+    }
+ }
+ u();
