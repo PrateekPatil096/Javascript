a34f022cd22061fc13bcc2e91123be52d967bac8
Prateek Patil <prateekpatil096@gmail.com>
2026-07-26 11:46:57 +0530

    shadowing in js

--- ch_8/scrpit.js
+++ ch_8/scrpit.js
@@ -1,3 +1,4 @@
+var a=100;
 {
     var a=10;
     let b=20;
@@ -7,5 +8,16 @@
     console.log(c);
 }
 console.log(a);
-console.log(b);
-console.log(c);
+//console.log(b);
+//console.log(c);
+
+let x=10;
+{
+    var y=10;
+    let x=20;
+    const z=30;
+    console.log(x);
+    console.log(y);
+    console.log(z);
+}
+console.log(x);
