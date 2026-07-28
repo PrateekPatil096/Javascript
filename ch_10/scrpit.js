d9736b7e252ba8d3829bc503e5c679ede286bb27
Prateek Patil <prateekpatil096@gmail.com>
2026-07-28 10:54:44 +0530

    set timeout in js

--- ch_10/scrpit.js
+++ ch_10/scrpit.js
@@ -1,8 +1,18 @@
-function x(){
+/*function x(){
     var i=1;
     setTimeout(function (){
         console.log(i);
     },3000 );
+    console.log("namaste js");
+}
+x();*/
+
+function x(){
+    for(var i=1;i<=5;i++){
+        setTimeout(function (){
+            console.log(i);
+        },i*1000);
+    }
     console.log("namaste js");
 }
 x();
