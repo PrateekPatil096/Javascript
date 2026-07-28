e6a1c5c9fb128e0b8caef14d1052f5017a0c3d4e
Prateek Patil <prateekpatil096@gmail.com>
2026-07-28 10:57:29 +0530

    settimeout and closures in js

--- ch_10/scrpit.js
+++ ch_10/scrpit.js
@@ -7,12 +7,26 @@
 }
 x();*/
 
-function x(){
+/*function x(){
     for(var i=1;i<=5;i++){
         setTimeout(function (){
             console.log(i);
         },i*1000);
     }
     console.log("namaste js");
+}
+x();*/
+
+function x(){
+    for(var i=1;i<=5;i++){
+        function close(x){
+            setTimeout(function (){
+                console.log(x);
+            },x*1000);
+        }
+        close(i);
+    }
+    console.log("namaste js")
+
 }
 x();
