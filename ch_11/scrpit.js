fb7949f59ce4df2e7942e844f1da7f84c0e1f089
Prateek Patil <prateekpatil096@gmail.com>
2026-07-29 12:14:22 +0530

    closures questions 2

--- ch_11/scrpit.js
+++ ch_11/scrpit.js
@@ -14,3 +14,20 @@ function outest(){
 let a=100;
 var close=(outest())("hello");
 close();
+
+
+
+function counter(){
+    var count=0;
+
+    return function incrementcounter(){
+        count++;
+        console.log(count);
+    }
+}
+var counter1=counter();
+counter1();
+counter1();
+
+ var counter2=counter();
+ counter2();
