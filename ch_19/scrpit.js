07cd73879c51d388c6874a6e8339357b97049049
Prateek Patil <prateekpatil096@gmail.com>
2026-08-03 19:35:27 +0530

    promises

--- ch_19/scrpit.js
+++ ch_19/scrpit.js
@@ -1,7 +1,13 @@
-const cart=["shoes","pants","shirts"];
+/*const cart=["shoes","pants","shirts"];
 
 const promise=createOrder(cart);
 
 promise.then(function (orderId){
     proceedToPayment(orderId);
-});
+});*/
+
+const GITHUB_API="https://github.com/PrateekPatil096/Javascript.git"
+
+const user=fetch(GITHUB_API);
+
+console.log(user);
