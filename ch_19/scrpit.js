d90dbfe4646b7cfddda8c112791ecf81ec551ae2
Prateek Patil <prateekpatil096@gmail.com>
2026-08-04 10:34:27 +0530

    promises chain

--- ch_19/scrpit.js
+++ ch_19/scrpit.js
@@ -11,3 +11,10 @@ const GITHUB_API="https://github.com/PrateekPatil096/Javascript.git"
 const user=fetch(GITHUB_API);
 
 console.log(user);
+
+const cart=["shoes","pants","shirts"];
+
+createOrder(cart)
+.then((orderId)=> proccedToPayemnt(orderId))
+.then((paymentInfo)=> showOrderSummary(paymentInfo))
+.then((paymentInfo)=> updateWalletBalance(paymentInfo));
