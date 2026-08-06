bda2d3629dcb4bfe6599807b0536ad23a8b5ef32
Prateek Patil <prateekpatil096@gmail.com>
2026-08-06 11:08:50 +0530

    p2 failing case

--- ch_21/scrpit.js
+++ ch_21/scrpit.js
@@ -4,7 +4,7 @@ const p1=new Promise((resolve,reject)=>{
 })
 
 const p2=new Promise((resolve,reject)=>{
-    setTimeout(()=>resolve("p2 success"),1000);
+    setTimeout(()=>reject("p2 fail"),1000);
 })
 
 const p3=new Promise((resolve,reject)=>{
@@ -14,3 +14,6 @@ const p3=new Promise((resolve,reject)=>{
 Promise.all([p1,p2,p3]).then((res)=>{
     console.log(res);
 })
+.catch((err)=>{
+    console.error(err);
+});
