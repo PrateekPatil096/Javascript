2de31d933e4dbc1b6bf9361225141163b7205926
Prateek Patil <prateekpatil096@gmail.com>
2026-08-07 10:46:37 +0530

    await working

--- ch_22/scrpit.js
+++ ch_22/scrpit.js
@@ -1,9 +1,27 @@
-const p=new Promise((resolve, reject)=>{
-    resolve("promise resloved value");
+const p1=new Promise((resolve, reject)=>{
+    setTimeout(()=>{
+        resolve("promise resloved value");
+    },10000)
+    
+});
+
+const p2=new Promise((resolve, reject)=>{
+    setTimeout(()=>{
+        resolve("promise resloved value2");
+    },5000)
+    
 });
 
 async function handlePromise(){
-    const val=await p;
+    console.log("hello world")
+    const val=await p1;
+    console.log("namaste js")
     console.log(val);
+
+    const val2=await p2;
+    console.log("namaste js2");
+    console.log(val2);
+
+
 }
     handlePromise();
